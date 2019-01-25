2db4516959d343f8b2f0d609380753607273efac
Josh Hadik <josh.hadik@gmail.com>
2019-01-24 19:09:52 -0600

    refactor locals_setter

--- lib/dynamic_text/locals_setter.rb
+++ lib/dynamic_text/locals_setter.rb
@@ -1,45 +1,66 @@
 class DynamicText::LocalsSetter
-  def set_dynamic_template_locals(resource, attribute, opts={})
-    Hash.new.tap do |locals|
+  def initialize
+    @locals = {}
+  end
+
+  def get_dynamic_locals(resource, attribute, opts={})
+    @locals.merge!(opts).tap do |locals|
       locals[:resource] = resource
       locals[:attribute] = attribute
-      locals[:value] = resource.send(attribute)
-      locals[:placeholder] = opts[:placeholder] || "Enter #{attribute}..."
-      locals[:resource_type] = expected_resource_type(resource)
-      locals[:dynamic_tag] =
-        default_dynamic_tag(locals[:resource_type], resource.id, attribute)
+      locals[:resource_id] = resource.id
+      locals[:value] ||= resource.send(attribute)
+      locals[:resource_scope] ||= DynamicText.configuration.resource_scope
+      locals[:placeholder] ||= default_placeholder
+      locals[:resource_type] ||= default_resource_type
+      locals[:resource_route] ||= default_resource_route
+      locals[:dynamic_tag] ||= default_dynamic_tag
     end
   end
 
-  def set_editable_template_locals(resource, attribute, opts={})
-    set_dynamic_template_locals(resource, attribute, opts={}).tap do |locals|
-      opts[:url] ||= expected_path_for(locals[:resource_type], resource)
-      opts[:ajax_key] ||=
-        default_ajax_key(locals[:resource_type], attribute)
-      locals[:url] = opts[:url]
-      locals[:ajax_key] = opts[:ajax_key]
+  def get_editable_locals(resource, attribute, opts={})
+    get_dynamic_locals(resource, attribute, opts).tap do |locals|
+      locals[:url] ||= default_url
+      locals[:ajax_key] ||= default_ajax_key
     end
   end
 
   private
 
-  def resource_scope
-    @resource_scope ||= DynamicText.configuration.resource_scope
+  # Defaults
+
+  def default_placeholder
+    "Enter #{get_local(:attribute)}..."
   end
 
-  def expected_path_for(resource_type, resource)
-    "#{resource_scope}/#{resource_type.pluralize}/#{resource.id}"
+  def default_resource_type
+    get_local(:resource).class.name.downcase
   end
 
-  def expected_resource_type(resource)
-    resource.class.name.downcase
+  def default_resource_route
+    get_local(:resource_type).pluralize
   end
 
-  def default_dynamic_tag(resource_type, resource_id, attribute)
-    "#{resource_type}:#{resource_id}:#{attribute}"
+  def default_dynamic_tag
+    get_locals(:resource_type, :resource_id, :attribute).join(":")
   end
 
-  def default_ajax_key(resource_type, attribute)
-    "#{resource_type}:#{attribute}"
+  def default_url
+    get_locals(:resource_scope, :resource_route, :resource_id).join("/")
+  end
+
+  def default_ajax_key
+    get_locals(:resource_type, :attribute).join(":")
+  end
+
+  # Helpers
+
+  def get_local(local_name)
+    @locals[local_name]
+  end
+
+  def get_locals(*local_names)
+    local_names.collect do |local_name|
+      get_local(local_name)
+    end
   end
 end
--- lib/dynamic_text/view_renderer.rb
+++ lib/dynamic_text/view_renderer.rb
@@ -4,35 +4,33 @@ class DynamicText::ViewRenderer < AbstractController::Base
   end
 
   def render_dynamic_text_for(resource, attribute, opts={})
-    locals = dynamic_locals(resource, attribute, opts)
-    render_dynamic_text(**locals)
+    render_dynamic_text dynamic_locals(resource, attribute, opts)
   end
 
   def render_editable_text_for(resource, attribute, opts={})
-    locals = editable_locals(resource, attribute, opts)
-    render_editable_text(**locals)
+    render_editable_text editable_locals(resource, attribute, opts)
   end
 
   private
 
   def dynamic_locals(resource, attribute, opts={})
-    locals_setter.set_dynamic_template_locals(resource, attribute, opts)
+    locals_setter.get_dynamic_locals(resource, attribute, opts)
   end
 
   def editable_locals(resource, attribute, opts={})
-    locals_setter.set_editable_template_locals(resource, attribute, opts)
+    locals_setter.get_editable_locals(resource, attribute, opts)
   end
 
   def locals_setter
     DynamicText::LocalsSetter.new
   end
 
-  def render_editable_text(options)
-    render_partial(:editable_text, **options)
+  def render_editable_text(locals)
+    render_partial(:editable_text, **locals)
   end
 
-  def render_dynamic_text(options)
-    render_partial(:dynamic_text, **options)
+  def render_dynamic_text(locals)
+    render_partial(:dynamic_text, **locals)
   end
 
   def render_partial(partial, **locals)
